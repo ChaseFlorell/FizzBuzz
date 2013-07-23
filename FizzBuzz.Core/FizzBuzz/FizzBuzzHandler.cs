aea8c7b180f82de60e17812764b48ec04efdad68
Chase Florell <git@chaseflorell.com>
2013-07-23 15:30:29 -0600

    FizzBuzz should be Fizz * Buzz

--- FizzBuzz.Core/FizzBuzz/FizzBuzzHandler.cs
+++ FizzBuzz.Core/FizzBuzz/FizzBuzzHandler.cs
@@ -28,7 +28,7 @@ namespace FizzBuzz.Core.FizzBuzz
         {
             public static int Fizz { get { return 3; } }
             public static int Buzz { get { return 5; } }
-            public static int FizzBuzz { get { return 15; } }
+            public static int FizzBuzz { get { return Fizz * Buzz; } }
 
         }
     }
